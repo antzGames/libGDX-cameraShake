605258dc0531382435d4d061c7aebae0e1e8508b
Anthony Iantria <anthony.iantria@gmail.com>
2022-11-26 10:15:49 -0500

    Initial commit.

--- core/src/main/java/com/antz/camera/CameraShake.java
+++ core/src/main/java/com/antz/camera/CameraShake.java
@@ -77,7 +77,7 @@ public class CameraShake extends ApplicationAdapter {
 		batch.draw(image, -image.getWidth()/2f, -image.getHeight()/2f);
 
 		font.getData().setScale(2f);
-		font.draw(batch, "Click to re-shake!", -105f,-70f);
+		font.draw(batch, "Click to re-shake!", -110f,-70f);
 		font.getData().setScale(1f);
 
 		font.draw(batch, Stringf.format("Shake Radius: %.1f",shakeRadius) + Stringf.format("   minShakeRadius: %.1f", minimumShakeRadius) + Stringf.format("   fallOffFactor: %.2f",radiusFallOffFactor), -205f,+90f);
